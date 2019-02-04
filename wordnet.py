772502105ecce4d04614878e39bc6d16990b468d
svitlanama <alexandrovna.sveta@gmail.com>
2019-02-04 05:43:01 +0200

    Extract meronyms (part–whole) from term.

--- wordnet.py
+++ wordnet.py
@@ -1,17 +1,26 @@
 from textblob import Word
-word = Word("cat")
+word = Word("tree")
 word.synsets[:5]
 
 word.definitions[:5]
 #print(word.definitions[:5])
-term0 = word.synsets[0]
-term1 = word.synsets[1]
-term2 = word.synsets[2]
-term0.lemma_names
-term1.lemma_names
-term2.lemma_names
-term1.hypernyms()
-print("CAT")
-print(term0.hypernyms())
-print(term1.hypernyms())
-print(term2.hypernyms())
+term_h_0 = word.synsets[0]
+term_h_1 = word.synsets[1]
+term_h_2 = word.synsets[2]
+# term_h_0.lemma_names
+# term_h_1.lemma_names
+# tterm_h_2.lemma_names
+
+print("TREE")
+print("hyperonymy is-a")
+print(term_h_0.hypernyms())
+print(term_h_1.hypernyms())
+# print(term_h_2.hypernyms())
+print()
+print("meronymic part of")
+term_m_0 = word.synsets[0]
+# term_m_1 = word.synsets[1]
+# term_m_2 = word.synsets[2]
+print(term_m_0.part_meronyms())
+# print(term_m_1.part_meronyms())
+# print(term_m_2.part_meronyms())
